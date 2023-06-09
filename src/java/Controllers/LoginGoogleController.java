452ec1b8488a65b2e25e41b80f5cf8d1ee62764c
caothang7a7 <126409779+caothang7a7@users.noreply.github.com>
2023-06-09 08:41:31 +0700

    test

--- src/java/Controllers/LoginGoogleController.java
+++ src/java/Controllers/LoginGoogleController.java
@@ -15,6 +15,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
@@ -36,56 +37,57 @@ public class LoginGoogleController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    String code = request.getParameter("code");
-    String accessToken = getToken(code);
-    UserGoogleDto userGoogleDto = getUserInfo(accessToken);
-    
-    // Create a new User object
-    User user = new User();
-    
-    // Set the name and email values from userGoogleDto to user's username and userEmail fields respectively
-    user.setUserName(userGoogleDto.getName());
-    user.setUserEmail(userGoogleDto.getEmail());
-    
-    
-    // Add the new user to the database using UserFacade
-    UserFacade userFacade = new UserFacade();
-    if(userFacade.addUser(user)) {
-        request.setAttribute("message", "Login success.");
-        // Redirect to index page
-        request.getRequestDispatcher("/ocsn/index.do").forward(request, response);
-    } else {
-        
-        // Forward to login page with an error message
-        request.setAttribute("message", "Login fail.");
-        request.getRequestDispatcher("/login/login.do").forward(request, response);
-       
+        response.setContentType("text/html;charset=UTF-8");
+        String code = request.getParameter("code");
+        String accessToken = getToken(code);
+        UserGoogleDto userGoogleDto = getUserInfo(accessToken);
+
+        // Create a new User object
+        User user = new User();
+
+        // Set the name and email values from userGoogleDto to user's username and userEmail fields respectively
+        user.setUserName(userGoogleDto.getName());
+        user.setUserEmail(userGoogleDto.getEmail());
+
+        // Add the new user to the database using UserFacade
+        UserFacade userFacade = new UserFacade();
+        if (userFacade.addUser(user)) {
+            request.setAttribute("message", "Login success.");
+            HttpSession httpSession = request.getSession();
+            httpSession.setAttribute("User", user);
+            // Redirect to index page
+            request.getRequestDispatcher("/ocsn/index.do").forward(request, response);
+        } else {
+
+            // Forward to login page with an error message
+            request.setAttribute("message", "Login fail.");
+            request.getRequestDispatcher("/login/login.do").forward(request, response);
+
+        }
     }
-    }
-    
+
     public static String getToken(String code) throws ClientProtocolException, IOException {
-		// call api to get token
-		String response = Request.Post(Constants.GOOGLE_LINK_GET_TOKEN)
-				.bodyForm(Form.form().add("client_id", Constants.GOOGLE_CLIENT_ID)
-						.add("client_secret", Constants.GOOGLE_CLIENT_SECRET)
-						.add("redirect_uri", Constants.GOOGLE_REDIRECT_URI).add("code", code)
-						.add("grant_type", Constants.GOOGLE_GRANT_TYPE).build())
-				.execute().returnContent().asString();
+        // call api to get token
+        String response = Request.Post(Constants.GOOGLE_LINK_GET_TOKEN)
+                .bodyForm(Form.form().add("client_id", Constants.GOOGLE_CLIENT_ID)
+                        .add("client_secret", Constants.GOOGLE_CLIENT_SECRET)
+                        .add("redirect_uri", Constants.GOOGLE_REDIRECT_URI).add("code", code)
+                        .add("grant_type", Constants.GOOGLE_GRANT_TYPE).build())
+                .execute().returnContent().asString();
 
-		JsonObject jobj = new Gson().fromJson(response, JsonObject.class);
-		String accessToken = jobj.get("access_token").toString().replaceAll("\"", "");
-		return accessToken;
-	}
+        JsonObject jobj = new Gson().fromJson(response, JsonObject.class);
+        String accessToken = jobj.get("access_token").toString().replaceAll("\"", "");
+        return accessToken;
+    }
 
-	public static UserGoogleDto getUserInfo(final String accessToken) throws ClientProtocolException, IOException {
-		String link = Constants.GOOGLE_LINK_GET_USER_INFO + accessToken;
-		String response = Request.Get(link).execute().returnContent().asString();
+    public static UserGoogleDto getUserInfo(final String accessToken) throws ClientProtocolException, IOException {
+        String link = Constants.GOOGLE_LINK_GET_USER_INFO + accessToken;
+        String response = Request.Get(link).execute().returnContent().asString();
 
-		UserGoogleDto googlePojo = new Gson().fromJson(response, UserGoogleDto.class);
+        UserGoogleDto googlePojo = new Gson().fromJson(response, UserGoogleDto.class);
 
-		return googlePojo;
-	}
+        return googlePojo;
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
